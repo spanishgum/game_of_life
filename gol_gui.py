c4b507dd399e0a4a9d3a05ff1ca0cd4b6d744fa9
spanishgum <aps10d@my.fsu.edu>
2016-05-17 18:11:12 -0400

    Update gol_gui.py
    Made the canvas into a grid of rectangles. Added the game loop functionality to run game of life at 1 second intervals.

--- gol_gui.py
+++ gol_gui.py
@@ -1,38 +1,68 @@
-import Tkinter as tk
+import Tkinter as tk, time
 
 
 class gui:
-    def __init__(self, master):
-        frame = tk.Frame(master)
+    def __init__(self, rows, cols, game):
+        self.master = tk.Tk()
+        self.game = game
+        self.rows = rows
+        self.cols = cols
+        self.colors = ('black', 'white')
+        self.grid = [[0 for _ in range(self.rows)] for _ in range(self.cols)]
+        self.rects = [[None for _ in range(self.rows)] for _ in range(self.cols)]
+
+        frame = tk.Frame(self.master)
         frame.pack()
-        self.button = tk.Button(
-            frame, text='Quit', fg='blue', command=frame.quit)
-        self.button.pack(side=tk.LEFT)
-        self.title = tk.Button(
-            frame, text='Solr Population for SAMOS data in DOMS', 
-            command=self.say_hi)
-        self.title.pack(side=tk.LEFT)
-        self.canvas = tk.Canvas(master, width=200, height=100)
+
+        self.quit_button = tk.Button(frame, text='Quit', fg='blue', command=frame.quit)
+        self.quit_button.pack(side=tk.RIGHT)
+
+        self.title_button = tk.Button(frame, text='Game of Life', command=self.beg)
+        self.title_button.pack(side=tk.LEFT)
+
+        self.canvas = tk.Canvas(self.master, width=20*self.cols, height=20*self.rows, borderwidth=5, background='white')
         self.canvas.pack()
-        self.lineA = self.canvas.create_line(0, 0, 200, 100)
-        self.lineB = self.canvas.create_line(0, 100, 200, 0, fill='red', dash=(4, 4))
-        self.rectA = self.canvas.create_rectangle(50, 25, 150, 75, fill='blue')
+        self.canvas.bind('<Button-1>', self.click_handler)
+        self.master.update()
+
+        self.col_w = self.canvas.winfo_width() / self.cols
+        self.row_h = self.canvas.winfo_height() / self.rows
+        self.init_grid()
+
+        self.master.mainloop()
 
 
-    def say_hi(self):
-        print 'hi there, everyone!'
-        self.canvas.delete(self.lineA)
-        self.canvas.delete(self.lineB)
-        self.canvas.delete(self.rectA)
+    def beg(self):
+        self.game.set_grid(self.grid)
+        for _ in range(20):
+            self.game.iter()
+            self.update(self.game.get_grid())
+            self.master.update()
+            time.sleep(1)
 
-def main():
-    root = tk.Tk()
-    g = gui(root)
-    root.mainloop()
-    root.destroy()
+    def end(self):
+        self.master.destroy()
 
+    def init_grid(self):
+        for c in range(self.cols):
+            for r in range(self.rows):
+                self.rects[c][r] = self.canvas.create_rectangle(c * self.col_w, r * self.row_h, (c+1) * self.col_w, (r+1) * self.row_h, fill = self.colors[self.grid[c][r]])
 
+    def update_cell(self, col, row):
+        self.canvas.itemconfig(self.rects[col][row], fill = self.colors[self.grid[col][row]])
 
+    def update(self, grid):
+        self.grid = grid
+        for c in range(self.cols):
+            for r in range(self.rows):
+                self.update_cell(c, r)
+
+    def click_handler(self, event):
+        col = event.x / self.col_w
+        row = event.y / self.row_h
+        print col, row
+        self.grid[col][row] = 1 - self.grid[col][row]
+        self.canvas.itemconfig(self.rects[col][row], fill = self.colors[self.grid[col][row]])
 
 if __name__ == '__main__':
-    main()
+    gui(10, 10, None).beg()
