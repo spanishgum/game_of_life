150a15e502579cf11ac805cd5fb1ee9062e46350
spanishgum <aps10d@my.fsu.edu>
2016-05-17 18:11:59 -0400

    Update gol_thd.py
    went back to sequential to get the gui up and running. Threading will have to wai for now.

--- gol_thd.py
+++ gol_thd.py
@@ -21,41 +21,27 @@ class gol:
                 self.out.write(str(val))
             self.out.write('\n')
     
-    def iter(self):
-        tpool = TP(self.n_thds)
-        beg, end, count = 0, 0, 0
-        thd_dat = []
-        for i in range(self.n_thds):
-            thd_dat.append({ 'beg' : beg, 'cells' : self.cells_per_thd, 'id' : None })
-            if i < self.xtra_cells:
-                beg += 1
-                thd_dat[-1]['cells'] += 1
-        tpool.map(self.calculate, thd_dat)
-        tpool.close()
-        tpool.join()
-        tpool.map(self.update, thd_dat)
-        tpool.close()
-        tpool.join()
-        
-    # def iter(self):
-        # beg, end, count = 0, 0, 0
-        # thd_dat = [None] * self.n_thds
-        # for i in range(self.n_thds):
-            # thd_dat[i] = { 'beg' : beg, 'cells' : self.cells_per_thd, 'id' : None }
-            # if i < self.xtra_cells:
-                # beg += 1
-                # thd_dat[i]['cells'] += 1
-            # thd_dat[i]['id'] = threading.Thread(target = self.calculate, args = (thd_dat[i],))
-            # thd_dat[i]['id'].start()
-        # for i in range(self.n_thds):
-            # thd_dat[i]['id'].join()
-        # for i in range(self.n_thds):
-            # thd_dat[i]['id'] = threading.Thread(target = self.update, args = (thd_dat[i],))
-            # thd_dat[i]['id'].start()
-        # for i in range(self.n_thds):
-            # thd_dat[i]['id'].join()
+    def get_grid(self):
+        return self.grid
 
-    def ncount(x, y):
+    def set_grid(self, g):
+        self.grid = g
+
+    def iter(self):
+        for y in range(self.y):
+            for x in range(self.x):
+                self.update_cell(x, y, self.ncount(x, y))
+        self.grid = self.newgrid
+    
+    def update_cell(self, x, y, n):
+        if n <= 1 or n >= 4:
+            self.newgrid[y][x] = 0
+        elif n == 3:
+            self.newgrid[y][x] = 1
+        else:
+            self.newgrid[y][x] = self.grid[y][x]
+
+    def ncount(self, x, y):
         g = self.grid
         if not (0 <= x < self.x) or not (0 <= y < self.y):
             raise Exception
@@ -82,40 +68,43 @@ class gol:
                 n = g[y-1][x-1] + g[y][x-1] + g[y+1][x-1] + g[y-1][x] + g[y+1][x] + g[y-1][x+1] + g[y][x+1] + g[y+1][x+1]
         return n
 
-    def calculate(beg, cells):
-        x = int(beg % self.x)
-        y = int(beg / self.x)
-        for i in range(cells):
-            c = self.ncount(x, y)
-            if c <= 1 or c >= 4:
-                self.newgrid[y][x] = 0
-            elif c == 3:
-                self.newgrid[y][x] = 1
-            else:
-                self.newgrid[y][x] = self.grid[y][x]
-            x += 1
-            if x == self.x:
-                x = 0
-                y += 1
+    # def calculate(self, beg, cells):
+        # x = int(beg % self.x)
+        # y = int(beg / self.x)
+        # for i in range(cells):
+            # c = self.ncount(x, y)
+            # if c <= 1 or c >= 4:
+                # self.newgrid[y][x] = 0
+            # elif c == 3:
+                # self.newgrid[y][x] = 1
+            # else:
+                # self.newgrid[y][x] = self.grid[y][x]
+            # x += 1
+            # if x == self.x:
+                # x = 0
+                # y += 1
     
-    def update(beg, cells):
-        x = int(beg % self.x)
-        y = int(beg / self.x)
-        for i in range(cells):
-            self.grid[y][x] = self.newgrid[y][x]
-            x += 1
-            if x == self.x:
-                x = 0
-                y += 1
+    # def update(self, beg, cells):
+        # x = int(beg % self.x)
+        # y = int(beg / self.x)
+        # for i in range(cells):
+            # self.grid[y][x] = self.newgrid[y][x]
+            # x += 1
+            # if x == self.x:
+                # x = 0
+                # y += 1
 
 
-
-def main():
+if __name__ == '__main__':
     g = gol(10, 10, 1)
     g.show()
     print '---------------'
-    g.iter()
+    g.grid[0][0] = 1
+    g.grid[0][1] = 1
+    g.grid[1][1] = 1
+    g.grid[1][2] = 1
     g.show()
-
-if __name__ == '__main__':
-    main()
+    for i in range(3):
+        print '---------------'
+        g.iter()
+        g.show()
